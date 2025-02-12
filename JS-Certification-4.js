67452414cab79be430e97b204e33c09cb8c7603b
KolveitsStudio <kolveitsstudio@gmail.com>
2025-02-12 14:34:57 +0100

    Create JS-Certification-4.js

---
+++ JS-Certification-4.js
@@ -0,0 +1,21 @@
+//TODO:
+// return change based on price of item
+// return the amount of cash provided
+// return amount of cash in the cash register
+// scenario when customer provides too little cash
+// scenario cash register doesnt have enough cash "Status: INSUFFICIENT_FUNDS"
+//scenario if cash register = amount due "Status: CLOSED"
+//scenario when cash register > amount due "Status: OPEN"
+
+let price = 1.87;
+let cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+];
