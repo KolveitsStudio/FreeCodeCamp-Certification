8c118339cd5c53f2f0acce550bd022eb3908842e
KolveitsStudio <kolveitsstudio@gmail.com>
2025-03-13 12:21:10 +0100

    Update React training.js

--- React training.js
+++ React training.js
@@ -10,3 +10,21 @@ class MyComponent extends React.Component {
     // Change code above this line
   }
 };
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
+
+// Change code below this line
