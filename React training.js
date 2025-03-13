d8198b9cb6b021c2148ed2417c3f734dd61e4c8b
KolveitsStudio <kolveitsstudio@gmail.com>
2025-03-13 13:36:21 +0100

    Update React training.js

--- React training.js
+++ React training.js
@@ -116,3 +116,58 @@ class ShoppingCart extends React.Component {
     { /* Change code above this line */ }
   }
 };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            { /* Change code below this line */ }
+            <Welcome name = {" "}/>
+            { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          { /* Change code below this line */ }
+          <p>Hello, <strong>{this.props.name}</strong>!</p>
+          { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// Change code below this line
+const Camper = (props) => {
+  return <p>{props.name}</p>
+}
+Camper.defaultProps = {
+  name: "CamperBot"
+};
+Camper.propTypes = {
+  name: PropTypes.string.isRequired
+};
