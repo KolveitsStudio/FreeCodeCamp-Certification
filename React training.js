f9ea0c4d7356e0f2d1de6aef6f8e57b0b49bcf27
KolveitsStudio <kolveitsstudio@gmail.com>
2025-03-21 14:21:10 +0100

    Update React training.js

--- React training.js
+++ React training.js
@@ -290,3 +290,56 @@ reset(){
     );
   }
 };
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+this.handleChange = this.handleChange.bind(this)
+  }
+handleChange(event){
+this.setState({input: event.target.value})
+}
+  render() {
+    return (
+      <div>
+        <input value={this.state.input} onChange={this.handleChange}></input>
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+event.preventDefault()
+    this.setState({submit: this.state.input})
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+
+          <input value={this.state.input} onChange={this.handleChange}></input>
+          <button type='submit'>Submit!</button>
+        </form>
+          <h1>{this.state.submit}</h1>
+      </div>
+    );
+  }
+}
